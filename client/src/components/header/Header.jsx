ef61116aa603b33a054d98fb30eb7ad4a64c1f86
unknown <sofia.iman@strathmore.edu>
2023-03-31 22:07:42 +0300

    homepage list

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -79,7 +79,7 @@ const Header = ({ type }) => {
         {type !== "list" && (
           <>
             <h1 className="headerTitle">
-              A lifetime of discounts? It's Genius.
+            Discount bliss for life
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,22 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/propertyList";
 import "./home.css";
+import FeaturedProperties from "../../components/featuredProperties/featuredProperties";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse Property By Type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Highly sought-after homes</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 };
